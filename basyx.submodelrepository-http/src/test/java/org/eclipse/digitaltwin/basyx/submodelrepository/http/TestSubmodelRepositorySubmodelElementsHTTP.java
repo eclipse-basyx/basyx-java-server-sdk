eclipse-basyx/basyx-java-components

Add create delete submodelElement REST API
Signed-off-by: Zai Zhang <zai.mueller-zhang@iese.fraunhofer.de>

basyx.submodelrepository-http/src/test/java/org/eclipse/digitaltwin/basyx/submodelrepository/http/TestSubmodelRepositorySubmodelElementsHTTP.java
@@ -102,7 +102,7 @@ public class TestSubmodelRepositorySubmodelElementsHTTP {
 	@Test
 	public void getPropertyValue() throws IOException, ParseException {
 		String expectedValue = wrapStringValue("5000");
-		
+
 		CloseableHttpResponse response = requestSubmodelElementValue(DummySubmodelFactory.SUBMODEL_TECHNICAL_DATA_ID, SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_PROPERTY_ID_SHORT);
 
 		assertEquals(HttpStatus.OK.value(), response.getCode());
@@ -204,7 +204,7 @@ public class TestSubmodelRepositorySubmodelElementsHTTP {
 
 		BaSyxHttpTestUtils.assertSameJSONContent(expectedValue, BaSyxHttpTestUtils.getResponseAsString(response));
 	}
-	
+
 	@Test
 	public void getBlobValue() throws IOException, ParseException {
 		CloseableHttpResponse response = requestSubmodelElementValue(DummySubmodelFactory.SUBMODEL_TECHNICAL_DATA_ID, SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_BLOB_ID_SHORT);
@@ -355,28 +355,63 @@ public class TestSubmodelRepositorySubmodelElementsHTTP {
 
 		BaSyxHttpTestUtils.assertSameJSONContent(expectedValue, BaSyxHttpTestUtils.getResponseAsString(response));
 	}
-	
+
 	@Test
-	public void getSubmodelElementListValue() throws IOException, ParseException {
-		CloseableHttpResponse response = requestSubmodelElementValue(DummySubmodelFactory.SUBMODEL_TECHNICAL_DATA_ID, SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_SUBMODEL_ELEMENT_LIST_ID_SHORT);
+	public void createSubmodelElementCollidingId() throws IOException {
+		String element = getJSONValueAsString("SubmodelElement.json");
+		CloseableHttpResponse createdResponse = BaSyxHttpTestUtils.executePostOnServer(createSubmodelElementsURL(DummySubmodelFactory.SUBMODEL_TECHNICAL_DATA_ID), element);
 
 		assertEquals(HttpStatus.OK.value(), response.getCode());
 
-		String expectedValue = getJSONValueAsString("value/expectedSubmodelElementListValue.json");
+	@Test
+	public void createSubmodelElement() throws FileNotFoundException, IOException, ParseException {
+		String element = getJSONValueAsString("SubmodelElement.json");
+		CloseableHttpResponse createdResponse = BaSyxHttpTestUtils.executePostOnServer(createSubmodelElementsURL(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID), element);
 
-		BaSyxHttpTestUtils.assertSameJSONContent(expectedValue, BaSyxHttpTestUtils.getResponseAsString(response));
+		CloseableHttpResponse fetchedResponse = requestSubmodelElement(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_PROPERTY_ID_SHORT);
+		assertEquals(HttpStatus.OK.value(), createdResponse.getCode());
+		BaSyxHttpTestUtils.assertSameJSONContent(element, BaSyxHttpTestUtils.getResponseAsString(fetchedResponse));
 	}
 
 	@Test
-	public void setSubmodelElementListValue() throws IOException, ParseException {
-		String expectedValue = getJSONValueAsString("value/setSubmodelElementListValue.json");
+	public void deleteSubmodelElement() throws FileNotFoundException, IOException, ParseException {
+		CloseableHttpResponse deleteResponse = BaSyxHttpTestUtils.executeDeleteOnURL(createSpecificSubmodelElementURL(DummySubmodelFactory.SUBMODEL_TECHNICAL_DATA_ID, SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_PROPERTY_ID_SHORT));
+		assertEquals(HttpStatus.OK.value(), deleteResponse.getCode());
 
-		CloseableHttpResponse writeResponse = writeSubmodelElementValue(DummySubmodelFactory.SUBMODEL_TECHNICAL_DATA_ID, SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_SUBMODEL_ELEMENT_LIST_ID_SHORT, expectedValue);
-		assertEquals(HttpStatus.OK.value(), writeResponse.getCode());
+		CloseableHttpResponse fetchedResponse = requestSubmodelElement(DummySubmodelFactory.SUBMODEL_TECHNICAL_DATA_ID, SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_PROPERTY_ID_SHORT);
+		assertEquals(HttpStatus.NOT_FOUND.value(), fetchedResponse.getCode());
+	}
 
-		CloseableHttpResponse response = requestSubmodelElementValue(DummySubmodelFactory.SUBMODEL_TECHNICAL_DATA_ID, SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_SUBMODEL_ELEMENT_LIST_ID_SHORT);
+	@Test
+	public void createNestedSubmodelElement() throws FileNotFoundException, IOException, ParseException {
+		String element = getJSONValueAsString("SubmodelElement.json");
+		CloseableHttpResponse createdInCollectionResponse = BaSyxHttpTestUtils.executePostOnServer(createSpecificSubmodelElementURL(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, DummySubmodelFactory.SUBMODEL_ELEMENT_COLLECTION_SIMPLE), element);
+		CloseableHttpResponse createdInListResponse = BaSyxHttpTestUtils.executePostOnServer(createSpecificSubmodelElementURL(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, DummySubmodelFactory.SUBMODEL_ELEMENT_LIST_SIMPLE), element);
+		assertEquals(HttpStatus.OK.value(), createdInCollectionResponse.getCode());
+		assertEquals(HttpStatus.OK.value(), createdInListResponse.getCode());
+		
+		CloseableHttpResponse fetchedNestedInCollectionResponse = requestSubmodelElement(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, createCollectionNestedIdShortPath(SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_PROPERTY_ID_SHORT));
+		BaSyxHttpTestUtils.assertSameJSONContent(element, BaSyxHttpTestUtils.getResponseAsString(fetchedNestedInCollectionResponse));
+		
+		CloseableHttpResponse fetchedNestedInListResponse = requestSubmodelElement(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, createListNestedIdShortPath(1));
+		BaSyxHttpTestUtils.assertSameJSONContent(element, BaSyxHttpTestUtils.getResponseAsString(fetchedNestedInListResponse));
+	}
 
-		BaSyxHttpTestUtils.assertSameJSONContent(expectedValue, BaSyxHttpTestUtils.getResponseAsString(response));
+	@Test
+	public void deleteNestedSubmodelElement() throws FileNotFoundException, IOException, ParseException {
+		String nestedIdShortPathInCollection = createCollectionNestedIdShortPath(DummySubmodelFactory.SUBMODEL_ELEMENT_FIRST_ID_SHORT);
+		CloseableHttpResponse deleteResponse = BaSyxHttpTestUtils.executeDeleteOnURL(createSpecificSubmodelElementURL(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, nestedIdShortPathInCollection));
+		assertEquals(HttpStatus.OK.value(), deleteResponse.getCode());
+
+		// delete first element of the submodel element list
+		String nestedIdShortSmeList = createListNestedIdShortPath(0);
+		BaSyxHttpTestUtils.executeDeleteOnURL(createSpecificSubmodelElementURL(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, nestedIdShortSmeList));
+		assertEquals(HttpStatus.OK.value(), deleteResponse.getCode());
+
+		CloseableHttpResponse fetchedNestedInCollectionResponse = requestSubmodelElement(DummySubmodelFactory.SUBMODEL_ELEMENT_COLLECTION_SIMPLE, DummySubmodelFactory.SUBMODEL_ELEMENT_SIMPLE_DATA_ID_SHORT);
+		assertEquals(HttpStatus.NOT_FOUND.value(), fetchedNestedInCollectionResponse.getCode());
+		CloseableHttpResponse fetchedNestedInListResponse = requestSubmodelElement(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, createListNestedIdShortPath(1));
+		assertEquals(HttpStatus.NOT_FOUND.value(), fetchedNestedInListResponse.getCode());
 	}
 
 	@Test
@@ -387,13 +422,13 @@ public class TestSubmodelRepositorySubmodelElementsHTTP {
 		assertEquals(HttpStatus.NOT_FOUND.value(), response.getCode());
 	}
 
-	@Test
-	public void setSubmodelElementValueOfNonExistingSubmodel() throws IOException {
-		String valueToWrite = getJSONValueAsString("value/setFileValue.json");
-
-		CloseableHttpResponse response = writeSubmodelElementValue("nonExisting", "doesNotMatter", valueToWrite);
-
-		assertEquals(HttpStatus.NOT_FOUND.value(), response.getCode());
+	private String createCollectionNestedIdShortPath(String idShort) {
+		return DummySubmodelFactory.SUBMODEL_ELEMENT_COLLECTION_SIMPLE + "." + idShort;
+	}
+	
+	
+	private String createListNestedIdShortPath(int index) {
+		return DummySubmodelFactory.SUBMODEL_ELEMENT_LIST_SIMPLE.concat("%5B"+index+"%5D");
 	}
 	
 	private String wrapStringValue(String value) {

basyx.submodelservice-core/src/test/java/org/eclipse/digitaltwin/basyx/submodelservice/DummySubmodelFactory.java
@@ -87,9 +87,16 @@ public class DummySubmodelFactory {
 	public static final String SUBMODEL_ELEMENT_FIRST_LIST = "FirstList";
 	public static final String SUBMODEL_ELEMENT_SECOND_LIST = "SecondList";
 	public static final String SUBMODEL_ELEMENT_COLLECTION_DEEP = "DeepCollection";
+	public static final String SUBMODEL_ELEMENT_LIST_SIMPLE = "SimpleList";
+
+	// SUBMODEL_SIMPLE_DATA_WITH_ONE_COLLECTION
+	public static final String SUBMODEL_SIMPLE_DATA_ID_SHORT = "simpleSubmodel001";
+	public static final String SUBMODEL_SIMPLE_DATA_ID = "simpleSubmodel001";
+	public static final String SUBMODEL_ELEMENT_SIMPLE_DATA_ID_SHORT = "elementToDelete";
+	
 
 	public static Collection<Submodel> getSubmodels() {
-		return Arrays.asList(createTechnicalDataSubmodel(), createOperationalDataSubmodel());
+		return Arrays.asList(createTechnicalDataSubmodel(), createOperationalDataSubmodel(), createSimpleDataSubmodelWithCollection());
 	}
 
 	public static Submodel createTechnicalDataSubmodel() {
@@ -104,6 +111,10 @@ public class DummySubmodelFactory {
 		return new DefaultSubmodel.Builder().kind(ModelingKind.INSTANCE).idShort(SUBMODEL_OPERATIONAL_DATA_ID_SHORT).id(SUBMODEL_OPERATIONAL_DATA_ID).submodelElements(new DefaultProperty.Builder().kind(ModelingKind.INSTANCE)
 				.semanticId(new DefaultReference.Builder().keys(new DefaultKey.Builder().type(KeyTypes.CONCEPT_DESCRIPTION).value(SUBMODEL_OPERATIONAL_DATA_SEMANTIC_ID_PROPERTY).build()).type(ReferenceTypes.GLOBAL_REFERENCE).build())
 				.idShort(SUBMODEL_OPERATIONAL_DATA_PROPERTY_ID_SHORT).category(SUBMODEL_OPERATIONAL_DATA_PROPERTY_CATEGORY).value(SUBMODEL_OPERATIONAL_DATA_PROPERTY_VALUE).valueType(DataTypeDefXsd.INTEGER).build()).build();
+	}
+
+	public static Submodel createSimpleDataSubmodelWithCollection() {
+		return new DefaultSubmodel.Builder().kind(ModelingKind.INSTANCE).idShort(SUBMODEL_SIMPLE_DATA_ID_SHORT).id(SUBMODEL_SIMPLE_DATA_ID).submodelElements(createSimpleSubmodelElements()).build();
 	}
 
 	public static Submodel createOperationalDataSubmodelWithHierarchicalSubmodelElements() {
@@ -179,6 +190,26 @@ public class DummySubmodelFactory {
 				.idShort(propertyIdShort).category(SUBMODEL_OPERATIONAL_DATA_PROPERTY_CATEGORY)
 				.value(SUBMODEL_OPERATIONAL_DATA_PROPERTY_VALUE).valueType(DataTypeDefXsd.INTEGER).build();
 		return sme3;
+	}
+
+	private static List<SubmodelElement> createSimpleSubmodelElements() {
+		ArrayList<SubmodelElement> list = new ArrayList<>();
+		Collection<SubmodelElement> smeColelctionValue = new HashSet<>();
+		List<SubmodelElement> smeListValue = new ArrayList<>();
+		SubmodelElementCollection smeCollection = new DefaultSubmodelElementCollection();
+		smeCollection.setIdShort(SUBMODEL_ELEMENT_COLLECTION_SIMPLE);
+		smeColelctionValue.add(createProperty(SUBMODEL_ELEMENT_FIRST_ID_SHORT));
+		
+		SubmodelElementList smeList = new DefaultSubmodelElementList();
+		smeList.setIdShort(SUBMODEL_ELEMENT_LIST_SIMPLE);
+		smeListValue.add(createProperty(SUBMODEL_ELEMENT_SECOND_ID_SHORT));
+		
+		smeCollection.setValue(smeColelctionValue);
+		smeList.setValue(smeListValue);
+		list.add(smeCollection);
+		list.add(smeList);
+		list.add(createProperty(SUBMODEL_ELEMENT_SIMPLE_DATA_ID_SHORT));
+		return list;
 	}
 
 }

eclipse-basyx/basyx-java-server-sdk

basyx.submodelrepository-http/src/test/java/org/eclipse/digitaltwin/basyx/submodelrepository/http/TestSubmodelRepositorySubmodelElementsHTTP.java
@@ -102,7 +102,7 @@ public class TestSubmodelRepositorySubmodelElementsHTTP {
 	@Test
 	public void getPropertyValue() throws IOException, ParseException {
 		String expectedValue = wrapStringValue("5000");
-		
+
 		CloseableHttpResponse response = requestSubmodelElementValue(DummySubmodelFactory.SUBMODEL_TECHNICAL_DATA_ID, SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_PROPERTY_ID_SHORT);
 
 		assertEquals(HttpStatus.OK.value(), response.getCode());
@@ -204,7 +204,7 @@ public class TestSubmodelRepositorySubmodelElementsHTTP {
 
 		BaSyxHttpTestUtils.assertSameJSONContent(expectedValue, BaSyxHttpTestUtils.getResponseAsString(response));
 	}
-	
+
 	@Test
 	public void getBlobValue() throws IOException, ParseException {
 		CloseableHttpResponse response = requestSubmodelElementValue(DummySubmodelFactory.SUBMODEL_TECHNICAL_DATA_ID, SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_BLOB_ID_SHORT);
@@ -380,6 +380,65 @@ public class TestSubmodelRepositorySubmodelElementsHTTP {
 	}
 
 	@Test
+	public void createSubmodelElementCollidingId() throws IOException {
+		String element = getJSONValueAsString("SubmodelElement.json");
+		CloseableHttpResponse createdResponse = BaSyxHttpTestUtils.executePostOnServer(createSubmodelElementsURL(DummySubmodelFactory.SUBMODEL_TECHNICAL_DATA_ID), element);
+
+		assertEquals(HttpStatus.CONFLICT.value(), createdResponse.getCode());
+	}
+
+	@Test
+	public void createSubmodelElement() throws FileNotFoundException, IOException, ParseException {
+		String element = getJSONValueAsString("SubmodelElement.json");
+		CloseableHttpResponse createdResponse = BaSyxHttpTestUtils.executePostOnServer(createSubmodelElementsURL(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID), element);
+
+		CloseableHttpResponse fetchedResponse = requestSubmodelElement(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_PROPERTY_ID_SHORT);
+		assertEquals(HttpStatus.OK.value(), createdResponse.getCode());
+		BaSyxHttpTestUtils.assertSameJSONContent(element, BaSyxHttpTestUtils.getResponseAsString(fetchedResponse));
+	}
+
+	@Test
+	public void deleteSubmodelElement() throws FileNotFoundException, IOException, ParseException {
+		CloseableHttpResponse deleteResponse = BaSyxHttpTestUtils.executeDeleteOnURL(createSpecificSubmodelElementURL(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, DummySubmodelFactory.SUBMODEL_ELEMENT_SIMPLE_DATA_ID_SHORT));
+		assertEquals(HttpStatus.OK.value(), deleteResponse.getCode());
+
+		CloseableHttpResponse fetchedResponse = requestSubmodelElement(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, DummySubmodelFactory.SUBMODEL_ELEMENT_SIMPLE_DATA_ID_SHORT);
+		assertEquals(HttpStatus.NOT_FOUND.value(), fetchedResponse.getCode());
+	}
+
+	@Test
+	public void createNestedSubmodelElement() throws FileNotFoundException, IOException, ParseException {
+		String element = getJSONValueAsString("SubmodelElement.json");
+		CloseableHttpResponse createdInCollectionResponse = BaSyxHttpTestUtils.executePostOnServer(createSpecificSubmodelElementURL(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, DummySubmodelFactory.SUBMODEL_ELEMENT_COLLECTION_SIMPLE), element);
+		CloseableHttpResponse createdInListResponse = BaSyxHttpTestUtils.executePostOnServer(createSpecificSubmodelElementURL(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, DummySubmodelFactory.SUBMODEL_ELEMENT_LIST_SIMPLE), element);
+		assertEquals(HttpStatus.OK.value(), createdInCollectionResponse.getCode());
+		assertEquals(HttpStatus.OK.value(), createdInListResponse.getCode());
+		
+		CloseableHttpResponse fetchedNestedInCollectionResponse = requestSubmodelElement(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, createCollectionNestedIdShortPath(SubmodelServiceHelper.SUBMODEL_TECHNICAL_DATA_PROPERTY_ID_SHORT));
+		BaSyxHttpTestUtils.assertSameJSONContent(element, BaSyxHttpTestUtils.getResponseAsString(fetchedNestedInCollectionResponse));
+		
+		CloseableHttpResponse fetchedNestedInListResponse = requestSubmodelElement(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, createListNestedIdShortPath(1));
+		BaSyxHttpTestUtils.assertSameJSONContent(element, BaSyxHttpTestUtils.getResponseAsString(fetchedNestedInListResponse));
+	}
+
+	@Test
+	public void deleteNestedSubmodelElement() throws FileNotFoundException, IOException, ParseException {
+		String nestedIdShortPathInCollection = createCollectionNestedIdShortPath(DummySubmodelFactory.SUBMODEL_ELEMENT_FIRST_ID_SHORT);
+		CloseableHttpResponse deleteResponse = BaSyxHttpTestUtils.executeDeleteOnURL(createSpecificSubmodelElementURL(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, nestedIdShortPathInCollection));
+		assertEquals(HttpStatus.OK.value(), deleteResponse.getCode());
+
+		// delete first element of the submodel element list
+		String nestedIdShortSmeList = createListNestedIdShortPath(0);
+		BaSyxHttpTestUtils.executeDeleteOnURL(createSpecificSubmodelElementURL(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, nestedIdShortSmeList));
+		assertEquals(HttpStatus.OK.value(), deleteResponse.getCode());
+
+		CloseableHttpResponse fetchedNestedInCollectionResponse = requestSubmodelElement(DummySubmodelFactory.SUBMODEL_ELEMENT_COLLECTION_SIMPLE, DummySubmodelFactory.SUBMODEL_ELEMENT_SIMPLE_DATA_ID_SHORT);
+		assertEquals(HttpStatus.NOT_FOUND.value(), fetchedNestedInCollectionResponse.getCode());
+		CloseableHttpResponse fetchedNestedInListResponse = requestSubmodelElement(DummySubmodelFactory.SUBMODEL_SIMPLE_DATA_ID, createListNestedIdShortPath(1));
+		assertEquals(HttpStatus.NOT_FOUND.value(), fetchedNestedInListResponse.getCode());
+	}
+
+	@Test
 	public void setNonExistingSubmodelElementValue() throws IOException {
 		String valueToWrite = getJSONValueAsString("value/setFileValue.json");
 
@@ -387,13 +446,13 @@ public class TestSubmodelRepositorySubmodelElementsHTTP {
 		assertEquals(HttpStatus.NOT_FOUND.value(), response.getCode());
 	}
 
-	@Test
-	public void setSubmodelElementValueOfNonExistingSubmodel() throws IOException {
-		String valueToWrite = getJSONValueAsString("value/setFileValue.json");
-
-		CloseableHttpResponse response = writeSubmodelElementValue("nonExisting", "doesNotMatter", valueToWrite);
-
-		assertEquals(HttpStatus.NOT_FOUND.value(), response.getCode());
+	private String createCollectionNestedIdShortPath(String idShort) {
+		return DummySubmodelFactory.SUBMODEL_ELEMENT_COLLECTION_SIMPLE + "." + idShort;
+	}
+	
+	
+	private String createListNestedIdShortPath(int index) {
+		return DummySubmodelFactory.SUBMODEL_ELEMENT_LIST_SIMPLE.concat("%5B"+index+"%5D");
 	}
 	
 	private String wrapStringValue(String value) {

basyx.submodelservice-core/src/test/java/org/eclipse/digitaltwin/basyx/submodelservice/DummySubmodelFactory.java
@@ -87,9 +87,17 @@ public class DummySubmodelFactory {
 	public static final String SUBMODEL_ELEMENT_FIRST_LIST = "FirstList";
 	public static final String SUBMODEL_ELEMENT_SECOND_LIST = "SecondList";
 	public static final String SUBMODEL_ELEMENT_COLLECTION_DEEP = "DeepCollection";
+	public static final String SUBMODEL_ELEMENT_LIST_SIMPLE = "SimpleList";
+	public static final String SUBMODEL_ELEMENT_COLLECTION_SIMPLE = "SimpleCollection";
+
+	// SUBMODEL_SIMPLE_DATA_WITH_ONE_COLLECTION
+	public static final String SUBMODEL_SIMPLE_DATA_ID_SHORT = "simpleSubmodel001";
+	public static final String SUBMODEL_SIMPLE_DATA_ID = "simpleSubmodel001";
+	public static final String SUBMODEL_ELEMENT_SIMPLE_DATA_ID_SHORT = "elementToDelete";
+	
 
 	public static Collection<Submodel> getSubmodels() {
-		return Arrays.asList(createTechnicalDataSubmodel(), createOperationalDataSubmodel());
+		return Arrays.asList(createTechnicalDataSubmodel(), createOperationalDataSubmodel(), createSimpleDataSubmodel());
 	}
 
 	public static Submodel createTechnicalDataSubmodel() {
@@ -104,6 +112,10 @@ public class DummySubmodelFactory {
 		return new DefaultSubmodel.Builder().kind(ModelingKind.INSTANCE).idShort(SUBMODEL_OPERATIONAL_DATA_ID_SHORT).id(SUBMODEL_OPERATIONAL_DATA_ID).submodelElements(new DefaultProperty.Builder().kind(ModelingKind.INSTANCE)
 				.semanticId(new DefaultReference.Builder().keys(new DefaultKey.Builder().type(KeyTypes.CONCEPT_DESCRIPTION).value(SUBMODEL_OPERATIONAL_DATA_SEMANTIC_ID_PROPERTY).build()).type(ReferenceTypes.GLOBAL_REFERENCE).build())
 				.idShort(SUBMODEL_OPERATIONAL_DATA_PROPERTY_ID_SHORT).category(SUBMODEL_OPERATIONAL_DATA_PROPERTY_CATEGORY).value(SUBMODEL_OPERATIONAL_DATA_PROPERTY_VALUE).valueType(DataTypeDefXsd.INTEGER).build()).build();
+	}
+
+	public static Submodel createSimpleDataSubmodel() {
+		return new DefaultSubmodel.Builder().kind(ModelingKind.INSTANCE).idShort(SUBMODEL_SIMPLE_DATA_ID_SHORT).id(SUBMODEL_SIMPLE_DATA_ID).submodelElements(createSimpleSubmodelElements()).build();
 	}
 
 	public static Submodel createOperationalDataSubmodelWithHierarchicalSubmodelElements() {
@@ -179,6 +191,26 @@ public class DummySubmodelFactory {
 				.idShort(propertyIdShort).category(SUBMODEL_OPERATIONAL_DATA_PROPERTY_CATEGORY)
 				.value(SUBMODEL_OPERATIONAL_DATA_PROPERTY_VALUE).valueType(DataTypeDefXsd.INTEGER).build();
 		return sme3;
+	}
+
+	private static List<SubmodelElement> createSimpleSubmodelElements() {
+		ArrayList<SubmodelElement> list = new ArrayList<>();
+		Collection<SubmodelElement> smeCollectionValue = new HashSet<>();
+		List<SubmodelElement> smeListValue = new ArrayList<>();
+		SubmodelElementCollection smeCollection = new DefaultSubmodelElementCollection();
+		smeCollection.setIdShort(SUBMODEL_ELEMENT_COLLECTION_SIMPLE);
+		smeCollectionValue.add(createProperty(SUBMODEL_ELEMENT_FIRST_ID_SHORT));
+		
+		SubmodelElementList smeList = new DefaultSubmodelElementList();
+		smeList.setIdShort(SUBMODEL_ELEMENT_LIST_SIMPLE);
+		smeListValue.add(createProperty(SUBMODEL_ELEMENT_SECOND_ID_SHORT));
+		
+		smeCollection.setValue(smeCollectionValue);
+		smeList.setValue(smeListValue);
+		list.add(smeCollection);
+		list.add(smeList);
+		list.add(createProperty(SUBMODEL_ELEMENT_SIMPLE_DATA_ID_SHORT));
+		return list;
 	}
 
 }