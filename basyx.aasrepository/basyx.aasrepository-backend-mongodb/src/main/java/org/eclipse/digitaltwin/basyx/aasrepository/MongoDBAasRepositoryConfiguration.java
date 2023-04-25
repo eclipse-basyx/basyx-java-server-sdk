3c8c495b61e7fd6b304e133a1c25426c150172fc
Frank Schnicke <frank.schnicke@iese.fraunhofer.de>
2023-04-25 11:36:12 +0200

    Fixes AAS Repo component for MongoDB
    Signed-off-by: Frank Schnicke <frank.schnicke@iese.fraunhofer.de>

--- basyx.aasrepository/basyx.aasrepository-backend-mongodb/src/main/java/org/eclipse/digitaltwin/basyx/aasrepository/MongoDBAasRepositoryConfiguration.java
+++ basyx.aasrepository/basyx.aasrepository-backend-mongodb/src/main/java/org/eclipse/digitaltwin/basyx/aasrepository/MongoDBAasRepositoryConfiguration.java
@@ -29,7 +29,6 @@ package org.eclipse.digitaltwin.basyx.aasrepository;
 import org.eclipse.digitaltwin.basyx.aasservice.AasServiceFactory;
 import org.eclipse.digitaltwin.basyx.aasservice.backend.InMemoryAasServiceFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -46,7 +45,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConditionalOnExpression("'${basyx.backend}'.equals('MongoDB')")
 public class MongoDBAasRepositoryConfiguration {
-	@ConditionalOnMissingBean
 	@Bean
 	public AasServiceFactory getAasServiceFactory() {
 		return new InMemoryAasServiceFactory();
