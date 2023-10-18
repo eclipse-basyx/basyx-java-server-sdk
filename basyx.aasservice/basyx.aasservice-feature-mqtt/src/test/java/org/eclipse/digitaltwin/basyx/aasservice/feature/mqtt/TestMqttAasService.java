9ec59611c5fe1ddc4dcef586f5eb842fcf1d097f
Mateus Molina <mateus.molina@iese.fraunhofer.de>
2023-10-18 11:19:47 +0200

    Fix TestMqttAasService.createMqttAasRepository() to use correct factory

--- basyx.aasservice/basyx.aasservice-feature-mqtt/src/test/java/org/eclipse/digitaltwin/basyx/aasservice/feature/mqtt/TestMqttAasService.java
+++ basyx.aasservice/basyx.aasservice-feature-mqtt/src/test/java/org/eclipse/digitaltwin/basyx/aasservice/feature/mqtt/TestMqttAasService.java
@@ -35,9 +35,10 @@ import org.eclipse.digitaltwin.aas4j.v3.model.AssetInformation;
 import org.eclipse.digitaltwin.aas4j.v3.model.AssetKind;
 import org.eclipse.digitaltwin.aas4j.v3.model.Reference;
 import org.eclipse.digitaltwin.aas4j.v3.model.impl.DefaultAssetInformation;
-import org.eclipse.digitaltwin.basyx.aasrepository.AasRepository;
-import org.eclipse.digitaltwin.basyx.aasrepository.DefaultAasRepositoryFactory;
 import org.eclipse.digitaltwin.basyx.aasrepository.AasInMemoryBackendProvider;
+import org.eclipse.digitaltwin.basyx.aasrepository.AasRepository;
+import org.eclipse.digitaltwin.basyx.aasrepository.AasRepositoryFactory;
+import org.eclipse.digitaltwin.basyx.aasrepository.DefaultAasRepositoryFactory;
 import org.eclipse.digitaltwin.basyx.aasservice.AasServiceFactory;
 import org.eclipse.digitaltwin.basyx.aasservice.AasServiceSuite;
 import org.eclipse.digitaltwin.basyx.aasservice.DummyAssetAdministrationShell;
@@ -157,8 +158,8 @@ public class TestMqttAasService extends AasServiceSuite {
 	}
 
 	private static AasRepository createMqttAasRepository() {
-		AasRepository repo = new DefaultAasRepositoryFactory(new AasInMemoryBackendProvider(), new InMemoryAasServiceFactory()).create();
-		return repo;
+		AasRepositoryFactory repoFactory = new DefaultAasRepositoryFactory(new AasInMemoryBackendProvider(), mqttAasServiceFactory);
+		return repoFactory.create();
 	}
 
 	private static MqttClient createAndConnectClient() throws MqttException, MqttSecurityException {
