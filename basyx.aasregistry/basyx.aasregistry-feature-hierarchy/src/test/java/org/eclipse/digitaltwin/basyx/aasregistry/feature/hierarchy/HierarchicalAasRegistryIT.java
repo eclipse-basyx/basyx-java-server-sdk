d1238c07c62f6926fe78eec0e501aebaa493fb95
Mateus Molina <mateus.molina@iese.fraunhofer.de>
2024-06-06 16:51:34 +0200

    Add example module

--- basyx.aasregistry/basyx.aasregistry-feature-hierarchy/src/test/java/org/eclipse/digitaltwin/basyx/aasregistry/feature/hierarchy/HierarchicalAasRegistryIT.java
+++ basyx.aasregistry/basyx.aasregistry-feature-hierarchy/src/test/java/org/eclipse/digitaltwin/basyx/aasregistry/feature/hierarchy/HierarchicalAasRegistryIT.java
@@ -54,7 +54,7 @@ import org.springframework.context.ConfigurableApplicationContext;
  * @author mateusmolina
  *
  */
-public class TestHierarchicalAasRegistry {
+abstract class HierarchicalAasRegistryIT {
 	private static final String DELEGATED_REGISTRY_URL = "http://localhost:8050";
 	private static final String REPO_BASE_URL = "http://127.0.0.1:8080";
 
