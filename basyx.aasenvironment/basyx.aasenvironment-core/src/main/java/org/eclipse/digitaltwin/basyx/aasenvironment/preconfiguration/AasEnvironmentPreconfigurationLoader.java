bd432a310cb1dca2d32add87b1adb9333377414a
FriedJannik <Jannik.Fried@iese.fraunhofer.de>
2024-07-11 13:59:20 +0200

    Refactors Preconfiguration Authorization

--- basyx.aasenvironment/basyx.aasenvironment-core/src/main/java/org/eclipse/digitaltwin/basyx/aasenvironment/preconfiguration/AasEnvironmentPreconfigurationLoader.java
+++ basyx.aasenvironment/basyx.aasenvironment-core/src/main/java/org/eclipse/digitaltwin/basyx/aasenvironment/preconfiguration/AasEnvironmentPreconfigurationLoader.java
@@ -32,34 +32,24 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.interfaces.RSAPublicKey;
 import java.util.ArrayList;
-import java.util.Collection;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.eclipse.digitaltwin.aas4j.v3.dataformat.core.DeserializationException;
 import org.eclipse.digitaltwin.basyx.aasenvironment.AasEnvironment;
 import org.eclipse.digitaltwin.basyx.aasenvironment.environmentloader.CompleteEnvironment;
 import org.eclipse.digitaltwin.basyx.aasenvironment.environmentloader.CompleteEnvironment.EnvironmentType;
-import org.eclipse.digitaltwin.basyx.authorization.DummyCredential;
-import org.eclipse.digitaltwin.basyx.authorization.DummyCredentialStore;
-import org.eclipse.digitaltwin.basyx.authorization.jwt.JwtTokenDecoder;
-import org.eclipse.digitaltwin.basyx.authorization.jwt.PublicKeyUtils;
-import org.eclipse.digitaltwin.basyx.client.internal.authorization.AccessTokenProviderFactory;
-import org.eclipse.digitaltwin.basyx.client.internal.authorization.TokenManager;
-import org.eclipse.digitaltwin.basyx.client.internal.authorization.grant.AccessTokenProvider;
-import org.eclipse.digitaltwin.basyx.client.internal.authorization.grant.GrantType;
+import org.eclipse.digitaltwin.basyx.authorization.CommonAuthorizationProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.integration.file.RecursiveDirectoryScanner;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 /**
@@ -68,7 +58,6 @@ import org.springframework.stereotype.Component;
  * @author fried, mateusmolina, despen, witt, jungjan, danish
  *
  */
-@Component
 public class AasEnvironmentPreconfigurationLoader {
 
 	private Logger logger = LoggerFactory.getLogger(AasEnvironmentPreconfigurationLoader.class);
@@ -78,37 +67,9 @@ public class AasEnvironmentPreconfigurationLoader {
 
 	private ResourceLoader resourceLoader;
 
-	@Value("${basyx.aasenvironment.authorization.preconfiguration.token-endpoint:#{null}}")
-	private String authenticationServerTokenEndpoint;
-
-	@Value("${basyx.aasenvironment.authorization.preconfiguration.client-id:#{null}}")
-	private String clientId;
-
-	@Value("${basyx.aasenvironment.authorization.preconfiguration.client-secret:#{null}}")
-	private String clientSecret;
-
-	@Value("${basyx.aasenvironment.authorization.preconfiguration.username:#{null}}")
-	private String username;
-
-	@Value("${basyx.aasenvironment.authorization.preconfiguration.password:#{null}}")
-	private String password;
-
-	@Value("${basyx.aasenvironment.authorization.preconfiguration.grant-type:#{null}}")
-	private String grantType;
-
-	@Value("${basyx.aasenvironment.authorization.preconfiguration.scopes:#{null}}")
-	private Collection<String> scopes;
-
-	@Value("${basyx.feature.authorization.enabled:false}")
-	private boolean authorizationEnabled;
-
-	private AccessTokenProvider tokenProvider;
-
-	@Autowired
 	public AasEnvironmentPreconfigurationLoader(ResourceLoader resourceLoader, List<String> pathsToLoad) {
 		this.resourceLoader = resourceLoader;
 		this.pathsToLoad = pathsToLoad;
-
 	}
 
 	public boolean shouldLoadPreconfiguredEnvironment() {
@@ -125,15 +86,10 @@ public class AasEnvironmentPreconfigurationLoader {
 		int filesCount = files.size();
 		int currenFileIndex = 0;
 
-		if(authorizationEnabled) {
-			setUpTokenProvider();
-			configureSecurityContext();
-		}
 		for (File file : files) {
 			logLoadingProcess(currenFileIndex++, filesCount, file.getName());
 			aasEnvironment.loadEnvironment(CompleteEnvironment.fromFile(file));
 		}
-		SecurityContextHolder.clearContext();
 	}
 
 	private List<File> scanForEnvironments(List<String> pathsToLoad) throws IOException {
@@ -184,32 +140,5 @@ public class AasEnvironmentPreconfigurationLoader {
 		logger.info("Loading AAS Environment ({}/{}) from file '{}'", current, overall, filename);
 	}
 
-	private void setUpTokenProvider() {
-		AccessTokenProviderFactory factory = new AccessTokenProviderFactory(GrantType.valueOf(grantType),scopes);
-		factory.setClientCredentials(clientId, clientSecret);
-		factory.setPasswordCredentials(username, password);
-		this.tokenProvider = factory.create();
-	}
-
-	private void configureSecurityContext() throws FileNotFoundException, IOException {
-		TokenManager tokenManager = new TokenManager(authenticationServerTokenEndpoint, tokenProvider);
-		String adminToken = tokenManager.getAccessToken();
-
-		String modulus = getStringFromFile("authorization/modulus.txt");
-		String exponent = "AQAB";
-
-		RSAPublicKey rsaPublicKey = PublicKeyUtils.buildPublicKey(modulus, exponent);
-
-		Jwt jwt = JwtTokenDecoder.decodeJwt(adminToken, rsaPublicKey);
-
-		SecurityContextHolder.getContext().setAuthentication(new JwtAuthenticationToken(jwt));
-	}
-
-
-	private String getStringFromFile(String fileName) throws FileNotFoundException, IOException {
-		ClassPathResource classPathResource = new ClassPathResource(fileName);
-		InputStream in = classPathResource.getInputStream();
-		return IOUtils.toString(in, StandardCharsets.UTF_8.name());
-	}
 
 }
--- basyx.aasenvironment/basyx.aasenvironment.component/src/main/java/org/eclipse/digitaltwin/basyx/aasenvironment/component/AasEnvironmentConfiguration.java
+++ basyx.aasenvironment/basyx.aasenvironment.component/src/main/java/org/eclipse/digitaltwin/basyx/aasenvironment/component/AasEnvironmentConfiguration.java
@@ -31,9 +31,11 @@ import org.eclipse.digitaltwin.basyx.aasenvironment.AasEnvironment;
 import org.eclipse.digitaltwin.basyx.aasenvironment.AasEnvironmentFactory;
 import org.eclipse.digitaltwin.basyx.aasenvironment.feature.AasEnvironmentFeature;
 import org.eclipse.digitaltwin.basyx.aasenvironment.feature.DecoratedAasEnvironmentFactory;
+import org.eclipse.digitaltwin.basyx.aasenvironment.preconfiguration.AasEnvironmentPreconfigurationLoader;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 
 /**
  * Configuration for aas environment for dependency injection
@@ -43,11 +45,17 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class AasEnvironmentConfiguration {
-	
+
 	@Bean
 	@ConditionalOnMissingBean
 	public static AasEnvironment getAasEnvironment(AasEnvironmentFactory aasEnvironmentFactory, List<AasEnvironmentFeature> features) {
 		return new DecoratedAasEnvironmentFactory(aasEnvironmentFactory, features).create();
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	public static AasEnvironmentPreconfigurationLoader getAasEnvironmentPreconfigurationLoader(ResourceLoader resourceLoader, List<String> pathsToLoad) {
+		return new AasEnvironmentPreconfigurationLoader(resourceLoader, pathsToLoad);
 	}
 
 }
