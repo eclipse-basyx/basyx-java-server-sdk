9272437a380e781b423ff5c08645c7aae57ff0fd
ShehriyarShariq-Fraunhofer <shehriyar.shariq@iese.fraunhofer.de>
2024-07-25 11:51:33 +0200

    fix: Test*DescriptorFactory refactor tests

--- basyx.submodelrepository/basyx.submodelrepository-feature-registry-integration/src/test/java/org/eclipse/digitaltwin/basyx/submodelrepository/feature/registry/integration/TestSubmodelDescriptorFactory.java
+++ basyx.submodelrepository/basyx.submodelrepository-feature-registry-integration/src/test/java/org/eclipse/digitaltwin/basyx/submodelrepository/feature/registry/integration/TestSubmodelDescriptorFactory.java
@@ -8,35 +8,15 @@ public class TestSubmodelDescriptorFactory {
 
 	private static final String SUBMODEL_REPO_URL = "http://localhost:8081";
 	
-
-	private static final String SUBMODEL_REPOSITORY_PATH_WITH_SLASH = "/submodels";
-	private static final String SUBMODEL_REPOSITORY_PATH_WITHOUT_SLASH = "submodels";
+	private static final String SUBMODEL_REPOSITORY_PATH = "submodels";
 
 	@Test
-    public void testUrlWithTrailingSlashAndPathWithoutLeadingSlash() {
-        String baseURLWithSlash = SUBMODEL_REPO_URL + "/";
-
-        assertEquals(baseURLWithSlash + SUBMODEL_REPOSITORY_PATH_WITHOUT_SLASH, SubmodelDescriptorFactory.createSubmodelRepositoryUrl(baseURLWithSlash));
-    }
-	
-	@Test
-    public void testUrlWithTrailingSlashAndPathWithLeadingSlash() {
-        String baseURLWithSlash = SUBMODEL_REPO_URL + "/";
-
-        assertEquals(baseURLWithSlash + SUBMODEL_REPOSITORY_PATH_WITHOUT_SLASH, SubmodelDescriptorFactory.createSubmodelRepositoryUrl(baseURLWithSlash));
+    public void testUrlWithTrailingSlash() {
+        assertEquals(SUBMODEL_REPO_URL + "/" + SUBMODEL_REPOSITORY_PATH, SubmodelDescriptorFactory.createSubmodelRepositoryUrl(SUBMODEL_REPO_URL + "/"));
     }
 
     @Test
-    public void testUrlWithoutTrailingSlashAndPathWithoutLeadingSlash() {
-        String baseURLWithoutSlash = SUBMODEL_REPO_URL;
-
-        assertEquals(baseURLWithoutSlash + SUBMODEL_REPOSITORY_PATH_WITH_SLASH , SubmodelDescriptorFactory.createSubmodelRepositoryUrl(baseURLWithoutSlash));
-    }
-    
-    @Test
-    public void testUrlWithoutTrailingSlashAndPathWithLeadingSlash() {
-        String baseURLWithoutSlash = SUBMODEL_REPO_URL;
-
-        assertEquals(baseURLWithoutSlash + SUBMODEL_REPOSITORY_PATH_WITH_SLASH , SubmodelDescriptorFactory.createSubmodelRepositoryUrl(baseURLWithoutSlash));
+    public void testUrlWithoutTrailingSlash() {
+        assertEquals(SUBMODEL_REPO_URL + "/" + SUBMODEL_REPOSITORY_PATH , SubmodelDescriptorFactory.createSubmodelRepositoryUrl(SUBMODEL_REPO_URL));
     }
 }
