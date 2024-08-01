3306f65a9d86f3b1cc05c26beeec8ffe19b5bbe6
ShehriyarShariq-Fraunhofer <shehriyar.shariq@iese.fraunhofer.de>
2024-08-01 12:23:21 +0200

    refactor: reformat RepositoryUrlHelper + Test

--- basyx.common/basyx.core/src/main/java/org/eclipse/digitaltwin/basyx/core/RepositoryUrlHelper.java
+++ basyx.common/basyx.core/src/main/java/org/eclipse/digitaltwin/basyx/core/RepositoryUrlHelper.java
@@ -9,22 +9,17 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class RepositoryUrlHelper {
 
 	public static String createRepositoryUrl(String baseUrl, String additionalPath) {
-        try {
-            URI baseUri = new URI(baseUrl);
-    
-            URI finalUri = UriComponentsBuilder.newInstance()
-                    .scheme(baseUri.getScheme())
-                    .host(baseUri.getHost())
-                    .port(baseUri.getPort())
-                    .pathSegment(baseUri.getPath().replaceAll("^/|/$", "").split("/"))
-                    .pathSegment(additionalPath.replaceAll("^/|/$", "").split("/"))
-                    .build()
-                    .toUri();
-    
-            return finalUri.toURL().toString();
-        } catch (URISyntaxException | MalformedURLException e) {
-            throw new RuntimeException("The Base URL or additional path is malformed.\n" + e.getMessage(), e);
-        }
+		try {
+			URI baseUri = new URI(baseUrl);
+
+			URI finalUri = UriComponentsBuilder.newInstance().scheme(baseUri.getScheme()).host(baseUri.getHost())
+					.port(baseUri.getPort()).pathSegment(baseUri.getPath().replaceAll("^/|/$", "").split("/"))
+					.pathSegment(additionalPath.replaceAll("^/|/$", "").split("/")).build().toUri();
+
+			return finalUri.toURL().toString();
+		} catch (URISyntaxException | MalformedURLException e) {
+			throw new RuntimeException("The Base URL or additional path is malformed.\n" + e.getMessage(), e);
+		}
 	}
-	
+
 }
--- basyx.common/basyx.core/src/test/java/org/eclipse/digitaltwin/basyx/core/TestRepositoryUrlHelper.java
+++ basyx.common/basyx.core/src/test/java/org/eclipse/digitaltwin/basyx/core/TestRepositoryUrlHelper.java
@@ -9,47 +9,51 @@ public class TestRepositoryUrlHelper {
 	private static final String BASE_URL = "http://localhost:8081";
 	private static final String BASE_URL_WITH_CONTEXT = "http://localhost:8081/context";
 	private static final String ADDITIONAL_PATH = "shells";
-	
+
 	private static final String EXPECTED_URL = BASE_URL + "/" + ADDITIONAL_PATH;
 	private static final String EXPECTED_URL_WITH_CONTEXT = BASE_URL_WITH_CONTEXT + "/" + ADDITIONAL_PATH;
 
 	@Test
-    public void testUrlWithTrailingSlashAndPathNameWithNoLeadingSlash() {
+	public void testUrlWithTrailingSlashAndPathNameWithNoLeadingSlash() {
 		assertEquals(EXPECTED_URL, RepositoryUrlHelper.createRepositoryUrl(BASE_URL + "/", ADDITIONAL_PATH));
-    }
-	
-    @Test
-    public void testUrlWithoutTrailingSlashAndPathNameWithNoLeadingSlash() {
-    	assertEquals(EXPECTED_URL, RepositoryUrlHelper.createRepositoryUrl(BASE_URL, ADDITIONAL_PATH));
-    }
-    
-    @Test
-    public void testUrlWithTrailingSlashAndPathNameWithLeadingSlash() {
-    	assertEquals(EXPECTED_URL, RepositoryUrlHelper.createRepositoryUrl(BASE_URL + "/", "/" + ADDITIONAL_PATH));
-    }
-	
-    @Test
-    public void testUrlWithoutTrailingSlashAndPathNameWithLeadingSlash() {
-    	assertEquals(EXPECTED_URL, RepositoryUrlHelper.createRepositoryUrl(BASE_URL, "/" + ADDITIONAL_PATH));
-    }
-    
-    @Test
-    public void testUrlWithContextAndTrailingSlashAndPathNameWithLeadingSlash() {
-    	assertEquals(EXPECTED_URL_WITH_CONTEXT, RepositoryUrlHelper.createRepositoryUrl(BASE_URL_WITH_CONTEXT + "/", "/" + ADDITIONAL_PATH));
-    }
-    
-    @Test
-    public void testUrlWithContextAndNoTrailingSlashAndPathNameWithLeadingSlash() {
-    	assertEquals(EXPECTED_URL_WITH_CONTEXT, RepositoryUrlHelper.createRepositoryUrl(BASE_URL_WITH_CONTEXT, "/" + ADDITIONAL_PATH));
-    }
-    
-    @Test
-    public void testUrlWithContextAndTrailingSlashAndPathNameWithNoLeadingSlash() {
-    	assertEquals(EXPECTED_URL_WITH_CONTEXT, RepositoryUrlHelper.createRepositoryUrl(BASE_URL_WITH_CONTEXT + "/", ADDITIONAL_PATH));
-    }
-    
-    @Test
-    public void testUrlWithContextAndNoTrailingSlashAndPathNameWithNoLeadingSlash() {
-    	assertEquals(EXPECTED_URL_WITH_CONTEXT, RepositoryUrlHelper.createRepositoryUrl(BASE_URL_WITH_CONTEXT, ADDITIONAL_PATH));
-    }
+	}
+
+	@Test
+	public void testUrlWithoutTrailingSlashAndPathNameWithNoLeadingSlash() {
+		assertEquals(EXPECTED_URL, RepositoryUrlHelper.createRepositoryUrl(BASE_URL, ADDITIONAL_PATH));
+	}
+
+	@Test
+	public void testUrlWithTrailingSlashAndPathNameWithLeadingSlash() {
+		assertEquals(EXPECTED_URL, RepositoryUrlHelper.createRepositoryUrl(BASE_URL + "/", "/" + ADDITIONAL_PATH));
+	}
+
+	@Test
+	public void testUrlWithoutTrailingSlashAndPathNameWithLeadingSlash() {
+		assertEquals(EXPECTED_URL, RepositoryUrlHelper.createRepositoryUrl(BASE_URL, "/" + ADDITIONAL_PATH));
+	}
+
+	@Test
+	public void testUrlWithContextAndTrailingSlashAndPathNameWithLeadingSlash() {
+		assertEquals(EXPECTED_URL_WITH_CONTEXT,
+				RepositoryUrlHelper.createRepositoryUrl(BASE_URL_WITH_CONTEXT + "/", "/" + ADDITIONAL_PATH));
+	}
+
+	@Test
+	public void testUrlWithContextAndNoTrailingSlashAndPathNameWithLeadingSlash() {
+		assertEquals(EXPECTED_URL_WITH_CONTEXT,
+				RepositoryUrlHelper.createRepositoryUrl(BASE_URL_WITH_CONTEXT, "/" + ADDITIONAL_PATH));
+	}
+
+	@Test
+	public void testUrlWithContextAndTrailingSlashAndPathNameWithNoLeadingSlash() {
+		assertEquals(EXPECTED_URL_WITH_CONTEXT,
+				RepositoryUrlHelper.createRepositoryUrl(BASE_URL_WITH_CONTEXT + "/", ADDITIONAL_PATH));
+	}
+
+	@Test
+	public void testUrlWithContextAndNoTrailingSlashAndPathNameWithNoLeadingSlash() {
+		assertEquals(EXPECTED_URL_WITH_CONTEXT,
+				RepositoryUrlHelper.createRepositoryUrl(BASE_URL_WITH_CONTEXT, ADDITIONAL_PATH));
+	}
 }
