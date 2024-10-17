44fcb949b2547ccb0ed2957f2afe9cd26b98ef04
mateusmolina <mateus.molina@iese.fraunhofer.de>
2024-10-17 09:45:00 +0200

    wip: Remove spring dependency from ClientExample

--- examples/BaSyxClient/basyx-client/src/main/java/org/eclipse/digitaltwin/basyx/examples/basyxclient/Application.java
+++ examples/BaSyxClient/basyx-client/src/main/java/org/eclipse/digitaltwin/basyx/examples/basyxclient/Application.java
@@ -25,16 +25,47 @@
 
 package org.eclipse.digitaltwin.basyx.examples.basyxclient;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import java.nio.file.Path;
+import java.util.List;
 
-@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
+import org.eclipse.digitaltwin.basyx.aasenvironment.client.ConnectedAasManager;
+import org.eclipse.digitaltwin.basyx.examples.basyxclient.configuration.BasyxSettings;
+import org.eclipse.digitaltwin.basyx.examples.basyxclient.processing.BaSyxSyncService;
+import org.eclipse.digitaltwin.basyx.examples.basyxclient.processing.BaSyxWarehouseService;
+import org.eclipse.digitaltwin.basyx.examples.basyxclient.processing.DirectoryWatcher;
+import org.eclipse.digitaltwin.basyx.examples.basyxclient.processing.EntryProcessor;
+
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		BasyxSettings settings = buildBasyxSettingsFromEnv();
+		ConnectedAasManager connectedAasManager = buildConnectedAasManager(settings);
+		List<EntryProcessor> entryProcessors = buildEntryProcessors(connectedAasManager);
+
+		Path path = Path.of("/ingest");
+		DirectoryWatcher directoryWatcher = new DirectoryWatcher(entryProcessors, path);
+
+		Thread thread = new Thread(directoryWatcher);
+		thread.start();
 	}
 
+	static BasyxSettings buildBasyxSettingsFromEnv() {
+		String aasRegistryBaseUrl = System.getenv("AAS_REGISTRY_BASE_URL");
+		String aasRepositoryBaseUrl = System.getenv("AAS_REPOSITORY_BASE_URL");
+		String submodelRegistryBaseUrl = System.getenv("SUBMODEL_REGISTRY_BASE_URL");
+		String submodelRepositoryBaseUrl = System.getenv("SUBMODEL_REPOSITORY_BASE_URL");
+
+		return new BasyxSettings(aasRepositoryBaseUrl, submodelRepositoryBaseUrl, aasRegistryBaseUrl, submodelRegistryBaseUrl);
+	}
+
+	static ConnectedAasManager buildConnectedAasManager(BasyxSettings settings) {
+		return new ConnectedAasManager(settings.aasRegistryBaseUrl(), settings.aasRepositoryBaseUrl(), settings.submodelRegistryBaseUrl(), settings.submodelRepositoryBaseUrl());
+	}
+
+	static List<EntryProcessor> buildEntryProcessors(ConnectedAasManager connectedAasManager) {
+		BaSyxSyncService syncService = new BaSyxSyncService(connectedAasManager);
+		BaSyxWarehouseService warehouseService = new BaSyxWarehouseService(connectedAasManager);
+
+		return List.of(syncService, warehouseService);
+	}
 }
