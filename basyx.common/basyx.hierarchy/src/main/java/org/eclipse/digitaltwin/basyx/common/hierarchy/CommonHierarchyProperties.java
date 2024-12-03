124d0136e6981b8bb9509de754ac9bf87eaee4e6
mateusmolina <mateus.molina@iese.fraunhofer.de>
2024-12-03 11:25:40 +0100

    refactor: extract DelegationStrategy to common

---
+++ basyx.common/basyx.hierarchy/src/main/java/org/eclipse/digitaltwin/basyx/common/hierarchy/CommonHierarchyProperties.java
@@ -0,0 +1,11 @@
+package org.eclipse.digitaltwin.basyx.common.hierarchy;
+
+public final class CommonHierarchyProperties {
+
+    private CommonHierarchyProperties() {
+    }
+
+    public static final String HIERARCHY_FEATURE_NAME = "basyx.feature.hierarchy";
+    public static final String HIERARCHY_FEATURE_ENABLED = "basyx.feature.hierarchy.enabled";
+    public static final String HIERARCHY_FEATURE_DELEGATION_PREFIX = "basyx.feature.hierarchy.prefix";
+}
