ac5edf352b4a4c56c916688f2f8b4d0351aa05eb
Aaron Zielstorff <aaron.zielstorff@iese.fraunhofer.de>
2025-02-12 13:53:56 +0100

    Revert "Bump io.moquette:moquette-broker from 0.16 to 0.17 (#365)" (#626)
    This reverts commit 6b70322ece7353efc37ff247d23cff68438e1318.

--- basyx.submodelservice/basyx.submodelservice-feature-mqtt/src/test/java/org/eclipse/digitaltwin/basyx/submodelrepository/feature/mqtt/MqttTestListener.java
+++ basyx.submodelservice/basyx.submodelservice-feature-mqtt/src/test/java/org/eclipse/digitaltwin/basyx/submodelrepository/feature/mqtt/MqttTestListener.java
@@ -69,10 +69,6 @@ public class MqttTestListener implements InterceptHandler {
 	}
 
 	@Override
-	public void onSessionLoopError(Throwable cause) {
-	}
-
-	@Override
 	public void onDisconnect(InterceptDisconnectMessage arg0) {
 	}
 
