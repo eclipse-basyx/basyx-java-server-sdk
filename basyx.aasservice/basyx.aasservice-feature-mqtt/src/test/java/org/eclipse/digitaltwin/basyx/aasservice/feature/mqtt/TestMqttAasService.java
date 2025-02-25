ffe92f8750cd32f00c3545b701a8659eb6e2102c
Mateus Molina <mateus.molina@iese.fraunhofer.de>
2025-02-25 08:15:04 +0100

    tests: add missing assertTrue import

--- basyx.aasservice/basyx.aasservice-feature-mqtt/src/test/java/org/eclipse/digitaltwin/basyx/aasservice/feature/mqtt/TestMqttAasService.java
+++ basyx.aasservice/basyx.aasservice-feature-mqtt/src/test/java/org/eclipse/digitaltwin/basyx/aasservice/feature/mqtt/TestMqttAasService.java
@@ -66,6 +66,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestMqttAasService extends AasServiceSuite {
 
