217b2f575e03d055e9e6cb0d4f3d2a29b38aeb6d
Christoph Antes <christoph.antes@iese.fraunhofer.de>
2025-03-06 13:58:57 +0100

    Add overwrite functionality for loadEnvironment() in DefaultAASEnvironment

--- basyx.aasenvironment/basyx.aasenvironment-core/src/main/java/org/eclipse/digitaltwin/basyx/aasenvironment/AasEnvironment.java
+++ basyx.aasenvironment/basyx.aasenvironment-core/src/main/java/org/eclipse/digitaltwin/basyx/aasenvironment/AasEnvironment.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright (C) 2023 the Eclipse BaSyx Authors
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including
@@ -8,10 +8,10 @@
  * distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -19,7 +19,7 @@
  * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * 
+ *
  * SPDX-License-Identifier: MIT
  ******************************************************************************/
 package org.eclipse.digitaltwin.basyx.aasenvironment;
@@ -32,7 +32,7 @@ import org.eclipse.digitaltwin.basyx.aasenvironment.environmentloader.CompleteEn
 
 /**
  * Specifies the overall AasEnvironment API
- * 
+ *
  * @author zhangzai, danish
  *
  */
@@ -40,7 +40,7 @@ public interface AasEnvironment {
 
 	/**
 	 * Generate serialization from given aas and
-	 * 
+	 *
 	 * @param aasIds
 	 * @param submodelIds
 	 * @param includeConceptDescriptions
@@ -52,6 +52,9 @@ public interface AasEnvironment {
 	public String createXMLAASEnvironmentSerialization(List<String> aasIds, List<String> submodelIds, boolean includeConceptDescriptions) throws SerializationException;
 
 	public byte[] createAASXAASEnvironmentSerialization(List<String> aasIds, List<String> submodelIds, boolean includeConceptDescriptions) throws SerializationException, IOException;
-	
-	public void loadEnvironment(CompleteEnvironment completeEnvironment);
+
+	public void loadEnvironment(CompleteEnvironment completeEnvironment, boolean overwrite);
+	default void loadEnvironment(CompleteEnvironment completeEnvironment){
+		loadEnvironment(completeEnvironment, false);
+	}
 }
--- basyx.aasenvironment/basyx.aasenvironment-core/src/main/java/org/eclipse/digitaltwin/basyx/aasenvironment/base/DefaultAASEnvironment.java
+++ basyx.aasenvironment/basyx.aasenvironment-core/src/main/java/org/eclipse/digitaltwin/basyx/aasenvironment/base/DefaultAASEnvironment.java
@@ -127,7 +127,7 @@ public class DefaultAASEnvironment implements AasEnvironment {
 			SubmodelElement submodelElement = stack.pop();
 
 			if (submodelElement instanceof File file) {
-                if(!isURL(file.getValue()))
+				if(!isURL(file.getValue()))
 					files.add(file);
 			} else if (submodelElement instanceof SubmodelElementCollection collection) {
 				stack.addAll(collection.getValue());
@@ -139,14 +139,16 @@ public class DefaultAASEnvironment implements AasEnvironment {
 		return files;
 	}
 
-	public void loadEnvironment(CompleteEnvironment completeEnvironment) {
+	public void loadEnvironment(CompleteEnvironment completeEnvironment, boolean overwrite) {
 		Environment environment = completeEnvironment.getEnvironment();
 		List<InMemoryFile> relatedFiles = completeEnvironment.getRelatedFiles();
 
 		if (environment == null)
 			return;
 
-		checker.assertNoDuplicateIds(environment);
+		if(!overwrite){
+			checker.assertNoDuplicateIds(environment);
+		}
 
 		createShellsOnRepositoryFromEnvironment(environment, relatedFiles);
 		createSubmodelsOnRepositoryFromEnvironment(environment, completeEnvironment.getRelatedFiles());
@@ -371,7 +373,7 @@ public class DefaultAASEnvironment implements AasEnvironment {
 				logger.error("Thumbnail file {} does not exist in the repository", thumbnail.getPath());
 			}
 		}
-    }
+	}
 
 	private static boolean isThumbnailSet(Resource thumbnail) {
 		return thumbnail != null && thumbnail.getPath() != null;
@@ -379,21 +381,21 @@ public class DefaultAASEnvironment implements AasEnvironment {
 
 	private static String getThumbnailPathInAASX(String path) {
 		try {
-            return "/" + getHashedFilePath(path) + "." + getFileExtensionFromPath(path);
-        } catch (NoSuchAlgorithmException e) {
+			return "/" + getHashedFilePath(path) + "." + getFileExtensionFromPath(path);
+		} catch (NoSuchAlgorithmException e) {
 			logger.error("Could not hash thumbnail path {}", path);
-            throw new RuntimeException(e);
-        }
-    }
+			throw new RuntimeException(e);
+		}
+	}
 
 	private static String getFilePathInAASX(String path) {
 		try {
-            return aasxFilePathPrefix + getHashedFilePath(path) + "." + getFileExtensionFromPath(path);
-        } catch (NoSuchAlgorithmException e) {
+			return aasxFilePathPrefix + getHashedFilePath(path) + "." + getFileExtensionFromPath(path);
+		} catch (NoSuchAlgorithmException e) {
 			logger.error("Could not hash file path {}", path);
-            throw new RuntimeException(e);
-        }
-    }
+			throw new RuntimeException(e);
+		}
+	}
 
 	private static String getFileExtensionFromPath(String file) {
 		String[] split = file.split("\\.");
