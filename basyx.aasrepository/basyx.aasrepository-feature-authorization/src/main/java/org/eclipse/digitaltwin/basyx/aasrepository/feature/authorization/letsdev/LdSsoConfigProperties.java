52e4703fa69fd87d3744bd7319e866adf0618d80
Sebastian Walter <sebastian.walter@letsdev.de>
2025-04-11 08:52:39 +0200

    add ld sso integration to components

---
+++ basyx.aasrepository/basyx.aasrepository-feature-authorization/src/main/java/org/eclipse/digitaltwin/basyx/aasrepository/feature/authorization/letsdev/LdSsoConfigProperties.java
@@ -0,0 +1,43 @@
+package org.eclipse.digitaltwin.basyx.aasrepository.feature.authorization.letsdev;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("ld-sso")
+public class LdSsoConfigProperties {
+
+    String baseUrl;
+    String audience;
+    Boolean debugEnabled = false;
+
+    public String getBaseUrl() {
+
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+
+        this.baseUrl = baseUrl;
+    }
+
+    public String getAudience() {
+
+        return audience;
+    }
+
+    public void setAudience(String audience) {
+
+        this.audience = audience;
+    }
+
+    public Boolean getDebugEnabled() {
+
+        return debugEnabled;
+    }
+
+    public void setDebugEnabled(Boolean debugEnabled) {
+
+        this.debugEnabled = debugEnabled;
+    }
+}
