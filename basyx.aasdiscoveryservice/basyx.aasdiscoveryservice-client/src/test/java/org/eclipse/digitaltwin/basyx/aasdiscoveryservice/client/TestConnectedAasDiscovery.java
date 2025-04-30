8c125d1cc1b1b3af36c9d0c6f84c8495a0b56288
FriedJannik <Jannik.Fried@iese.fraunhofer.de>
2025-04-30 09:16:37 +0200

    Adds Client for Discovery Service - TODO: Authorized Client

---
+++ basyx.aasdiscoveryservice/basyx.aasdiscoveryservice-client/src/test/java/org/eclipse/digitaltwin/basyx/aasdiscoveryservice/client/TestConnectedAasDiscovery.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (C) 2025 the Eclipse BaSyx Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * SPDX-License-Identifier: MIT
+ ******************************************************************************/
+
+package org.eclipse.digitaltwin.basyx.aasdiscoveryservice.client;
+
+import org.eclipse.digitaltwin.basyx.aasdiscoveryservice.core.AasDiscoveryService;
+import org.eclipse.digitaltwin.basyx.aasdiscoveryservice.core.AasDiscoveryServiceSuite;
+import org.junit.After;
+import org.junit.Before;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * Tests the ConnectedAasDiscoveryService
+ *
+ * @author fried
+ */
+public class TestConnectedAasDiscovery extends AasDiscoveryServiceSuite {
+
+    private static ConfigurableApplicationContext appContext;
+
+    @Before
+    public void startAasDiscoveryService() {
+        appContext = new SpringApplicationBuilder(DummyAasDiscoveryServiceComponent.class).run(new String[] {});
+    }
+
+    @After
+    public void stopAasDiscoveryService() {
+        appContext.close();
+    }
+
+    @Override
+    protected AasDiscoveryService getAasDiscoveryService() {
+        return new ConnectedAasDiscoveryService("http://localhost:7788");
+    }
+}
