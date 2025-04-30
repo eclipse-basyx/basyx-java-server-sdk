a860512282316599a16176c16c7d65b8f705b8f2
geso02 <gerhard.sonnenberg@dfki.de>
2025-04-30 10:14:05 +0200

    Add newlines and update License version

--- basyx.submodelservice/basyx.submodelservice-feature-registry-integration/src/main/java/org/eclipse/digitaltwin/basyx/submodelservice/feature/registry/integration/RegistryIntegrationSubmodelServiceFeature.java
+++ basyx.submodelservice/basyx.submodelservice-feature-registry-integration/src/main/java/org/eclipse/digitaltwin/basyx/submodelservice/feature/registry/integration/RegistryIntegrationSubmodelServiceFeature.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2024 DFKI GmbH (https://www.dfki.de/en/web)
+ * Copyright (C) 2025 DFKI GmbH (https://www.dfki.de/en/web)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
