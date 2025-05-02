706d92fbea7d1245cd7a472e95af4065797ac7b3
Jannik Fried <Jannik.Fried@iese.fraunhofer.de>
2025-05-02 11:19:30 +0200

    Update ConnectedSubmodelService.java

--- basyx.submodelservice/basyx.submodelservice-client/src/main/java/org/eclipse/digitaltwin/basyx/submodelservice/client/ConnectedSubmodelService.java
+++ basyx.submodelservice/basyx.submodelservice-client/src/main/java/org/eclipse/digitaltwin/basyx/submodelservice/client/ConnectedSubmodelService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2024-TEST the Eclipse BaSyx Authors
+ * Copyright (C) 2024-TEST-2 the Eclipse BaSyx Authors
  * 
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
