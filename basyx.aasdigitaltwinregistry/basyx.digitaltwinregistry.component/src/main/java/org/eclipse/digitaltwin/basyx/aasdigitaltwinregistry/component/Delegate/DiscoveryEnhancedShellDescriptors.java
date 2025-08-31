8cb1bf7142a8800a2abb4fba019470f3f082313f
faheem <faheem.bhatti@smart-systems-hub.de>
2025-08-31 20:15:51 +0200

    clean up

--- basyx.aasdigitaltwinregistry/basyx.digitaltwinregistry.component/src/main/java/org/eclipse/digitaltwin/basyx/aasdigitaltwinregistry/component/Delegate/DiscoveryEnhancedShellDescriptors.java
+++ basyx.aasdigitaltwinregistry/basyx.digitaltwinregistry.component/src/main/java/org/eclipse/digitaltwin/basyx/aasdigitaltwinregistry/component/Delegate/DiscoveryEnhancedShellDescriptors.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
 import java.util.Base64;
 import java.util.List;
 import java.util.stream.Collectors;
