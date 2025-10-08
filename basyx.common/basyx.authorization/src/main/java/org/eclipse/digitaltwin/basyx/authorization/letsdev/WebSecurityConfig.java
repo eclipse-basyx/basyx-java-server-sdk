d73b3170a537d886512704cce0d45d672ce17c01
Sebastian Walter <sebastian.walter@letsdev.de>
2025-10-08 12:13:58 +0200

    whitelist OPTIONS requests

--- basyx.common/basyx.authorization/src/main/java/org/eclipse/digitaltwin/basyx/authorization/letsdev/WebSecurityConfig.java
+++ basyx.common/basyx.authorization/src/main/java/org/eclipse/digitaltwin/basyx/authorization/letsdev/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package org.eclipse.digitaltwin.basyx.authorization.letsdev;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -54,8 +55,11 @@ public class WebSecurityConfig {
                     }
 
                     customizer
+                            .requestMatchers(HttpMethod.OPTIONS, "/**")
+                                .permitAll()
+
                             .anyRequest()
-                            .authenticated();
+                                .authenticated();
                 });
 
         return http.build();
