9a459187a0e3ed7094fdc30347270639906daed8
FaheemBhatti <50632975+FaheemBhatti@users.noreply.github.com>
2025-10-14 19:33:15 +0200

    Feature impr/digital twin registry (#859)
    * DTR initial commit
    
    * feature printer addition
    
    * custom documentation
    
    * adding SPDX-License-Identifier to the new added files
    
    * review suggested changes from Arno
    
    * UT's
    
    * feature aas digital twin registry
    
    * feature digital twin registry
    
    * log level from TRACE --> INFO
    
    * merge failure resolve
    
    * update parent pom to revert not needed changes
    
    * refactoring acc to the requested changes
    
    * docs updation
    
    * revert cosmetic changes on parent pom
    
    * remove db details
    
    * remove circular reference
    
    * removing circular ref
    
    * cosmetic changes revert
    
    * improve log statement in the DiscoveryIntegrationAasRegistrytest class
    
    * exclusion optimization and discovery behaviour adjust acc to actual
    
    * resolve test failures in the discovery feature integration, add discovery feature in aas registry variations
    
    * dtr module to InMemory from MongoDB to test workflows
    
    * updating the registry poms
    
    * pom updatin
    
    * pom updation
    
    * removing potential dependecies causing bean pollution
    
    * test properties for the aas registry digital twin feature
    
    ---------
    
    Co-authored-by: faheem <faheem.bhatti@smart-systems-hub.de>

---
+++ basyx.aasdigitaltwinregistry/basyx.digitaltwinregistry.component/src/main/java/org/eclipse/digitaltwin/basyx/digitaltwinregistry/component/documentation/DTRegistryApiDocumentationConfiguration.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (C) 2025 the Eclipse BaSyx Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * SPDX-License-Identifier: MIT
+ ******************************************************************************/
+
+package org.eclipse.digitaltwin.basyx.digitaltwinregistry.component.documentation;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.eclipse.digitaltwin.basyx.http.documentation.RepositoryApiDocumentationConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DTRegistryApiDocumentationConfiguration extends RepositoryApiDocumentationConfiguration {
+
+	private static final String TITLE = "BaSyx Digital Twin Registry";
+	private static final String DESCRIPTION = "BaSyx Digital Twin Registry API";
+
+	@Bean
+	@Primary
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(apiInfo());
+	}
+
+	@Override
+	protected Info apiInfo() {
+		return new Info().title(TITLE)
+				.description(DESCRIPTION)
+				.version(VERSION)
+				.contact(apiContact())
+				.license(apiLicence());
+	}
+}
